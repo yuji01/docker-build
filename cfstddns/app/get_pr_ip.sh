0c8c8509397287d1eb7f7b5c304719d457d30a3d
yuji01 <59014140+yuji01@users.noreply.github.com>
2024-10-01 07:42:12 +0800

    Update get_pr_ip.sh

--- cfstddns/app/get_pr_ip.sh
+++ cfstddns/app/get_pr_ip.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
-# 获取反代ip
-wget $PR_IP_ADDR -O $WORKDIR/pr_ip.txt && echo "成功获取反代ip"
+# 获取反代ip到 $WORKDIR/pr_ip.txt
+proxychains4 -q wget $PR_IP_ADDR -O $WORKDIR/pr_ip.txt && echo "成功获取反代ip"
