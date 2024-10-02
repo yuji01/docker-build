ff58e690b22c7fbd7e635bacbe017f7d4b0c6f87
yuji01 <59014140+yuji01@users.noreply.github.com>
2024-10-02 23:12:22 +0800

    Update start.sh

--- rclone/start.sh
+++ rclone/start.sh
@@ -1,6 +1,13 @@
 #! /bin/bash
 # rclone配置文件下载直链
-wget "" -O /app/rclone.conf
+wget --no-check-certificate "" -O /app/rclone.conf
+
+# rclone 启动，其中 --config /app/rclone.conf 不可改
+
+if [ ! -f /app/rclone.conf ]; then
+  echo "rclone.conf 配置文件不存在，程序退出";
+  exit 1;
+fi
 
 # rclone 启动，其中 --config /app/rclone.conf 不可改
 rclone --config /app/rclone.conf 
