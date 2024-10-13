67619349fd18b2285b8b52b78a8b2b370a9d1921
yuji01 <59014140+yuji01@users.noreply.github.com>
2024-10-13 18:28:00 +0800

    Update start.sh

--- rclone/start.sh
+++ rclone/start.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
 
 # rclone配置文件下载直链
-wget --no-check-certificate "$URL" -O /app/rclone.conf &&
+mkdir -p /root/.config/rclone/
+wget --no-check-certificate "$URL" -O /root/.config/rclone/rclone.conf &&
 
 # rclone 启动，其中 --config /app/rclone.conf 不可改
-rclone --config /app/rclone.conf 
+rclone
