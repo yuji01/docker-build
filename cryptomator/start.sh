2494d3511b08e1146904ec5fc356126dcb69b16e
yuji01 <59014140+yuji01@users.noreply.github.com>
2024-10-19 11:45:52 +0800

    Update start.sh

--- cryptomator/start.sh
+++ cryptomator/start.sh
@@ -3,9 +3,12 @@
 # rclone配置文件下载直链
 mkdir -p /root/.config/rclone/ &&
 wget --no-check-certificate "$URL" -O /root/.config/rclone/rclone.conf &&
+
+cat /root/.config/rclone/rclone.conf
+
 mkdir -p /rclone-mount
 
-# rclone 挂载，以后台运行
+# rclone 挂载，后台运行
 nohup rclone mount $REMOTE /rclone-mount \
   --allow-non-empty \
   --allow-other \
@@ -14,13 +17,12 @@ nohup rclone mount $REMOTE /rclone-mount \
   --vfs-cache-mode writes \
   --vfs-cache-max-size 1G \
   --vfs-read-chunk-size 5M \
-  --transfers 8 > rclone.log 2>&1 &
+  --transfers 8 &
 
-if [ $? -eq 0 ];then
+sleep 3
 
+# 运行解密程序，解密后的文件挂载到 /mnt
 java -jar /usr/bin/cryptomator.jar \
     --vault $VAULT_NAME=$VAULT_PATH --password $VAULT_NAME=$VAULT_PASS \
     --bind $VAULT_BIND --port $CRYPTOMATOR_PORT \
-    --fusemount $VAULT_MOUNT
-
-fi
+    --fusemount $VAULT_NAME=$VAULT_MOUNT
