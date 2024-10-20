270a2b396dc4e42fd9da0d586662a3ff3bb16b83
yuji01 <59014140+yuji01@users.noreply.github.com>
2024-10-20 20:44:09 +0800

    Update start.sh

--- cryptomator/start.sh
+++ cryptomator/start.sh
@@ -6,24 +6,28 @@ wget --no-check-certificate "$URL" -O /root/.config/rclone/rclone.conf &&
 
 # 显示 rclone 配置文件内容到日志
 cat /root/.config/rclone/rclone.conf
-
+cp /root/.config/rclone/rclone.conf /app/rclone.conf
 # 创建 rclone 挂载点
 mkdir -p /rclone-mount
 
 # 启动 rclone，并将输出重定向到 stdout
-rclone mount $REMOTE /rclone-mount \
+rclone --config /app/rclone.conf mount $REMOTE /rclone-mount \
   --allow-non-empty \
   --allow-other \
-  --buffer-size 64M \
   --cache-dir /cache \
   --vfs-cache-mode writes \
-  --vfs-cache-max-size 1G \
-  --vfs-read-chunk-size 5M \
-  --transfers 8 \
+  --buffer-size 256M \
+  --vfs-cache-max-age 1h \
+  --vfs-cache-max-size 2G \
+  --vfs-read-chunk-size 64M \
+  --vfs-read-chunk-size-limit 2G \
+  --transfers 16 \
+  --dir-cache-time 72h \
+  --fast-list \
   --log-level INFO > /proc/1/fd/1 2>&1 &
 
 # 捕捉 SIGTERM 信号，确保在容器关闭时卸载挂载点
-trap "fusermount -u /rclone-mount" SIGTERM
+trap "umount -u /rclone-mount" SIGTERM
 
 sleep 3
 
