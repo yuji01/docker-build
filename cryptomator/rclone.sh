828acc25b5e91545f01698191b845a27fecccce4
yuji01 <59014140+yuji01@users.noreply.github.com>
2024-10-23 19:33:45 +0800

    Update rclone.sh

--- cryptomator/rclone.sh
+++ cryptomator/rclone.sh
@@ -3,15 +3,6 @@
 # 捕捉 SIGTERM 信号，确保在容器关闭时卸载挂载点
 trap "fusermount -u /rclone-mount; exit" SIGTERM
 
-# 创建 rclone 配置目录并下载配置文件
-wget --no-check-certificate "$URL" -O /app/rclone.conf &&
-
-# 显示 rclone 配置文件内容到日志
-cat /app/rclone.conf
-
-# 创建 rclone 挂载点
-mkdir -p /rclone-mount
-
 # 启动 rclone，并将输出重定向到 stdout
 rclone --config /app/rclone.conf mount $REMOTE /rclone-mount \
   --allow-non-empty \
