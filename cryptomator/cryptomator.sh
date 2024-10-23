a3be8055dade84b31083bb4a52c325ecb5c23075
yuji01 <59014140+yuji01@users.noreply.github.com>
2024-10-23 19:16:46 +0800

    Update cryptomator.sh

--- cryptomator/cryptomator.sh
+++ cryptomator/cryptomator.sh
@@ -4,4 +4,4 @@
 java -jar /usr/bin/cryptomator.jar \
     --vault $VAULT_NAME=$VAULT_PATH --password $VAULT_NAME=$VAULT_PASS \
     --bind $VAULT_BIND --port $CRYPTOMATOR_PORT \
-    --fusemount $VAULT_NAME=$VAULT_MOUNT
+    --fusemount $VAULT_NAME=$VAULT_MOUNT > /proc/1/fd/1 2>&1
