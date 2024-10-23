41ee5338849cd304c221b5cfc293d26acf0fd3bb
yuji01 <59014140+yuji01@users.noreply.github.com>
2024-10-23 18:10:48 +0800

    Create rclone.sh

---
+++ rclone.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# 创建 rclone 配置目录并下载配置文件
+wget --no-check-certificate "$URL" -O /app/rclone.conf &&
+
+# 显示 rclone 配置文件内容到日志
+cat /app/rclone.conf
+
+# 创建 rclone 挂载点
+mkdir -p /rclone-mount
+
+# 启动 rclone，并将输出重定向到 stdout
+rclone --config /app/rclone.conf mount $REMOTE /rclone-mount \
+  --allow-non-empty \
+  --allow-other \
+  --cache-dir /cache \
+  --vfs-cache-mode writes \
+  --buffer-size 256M \
+  --vfs-cache-max-age 1h \
+  --vfs-cache-max-size 2G \
+  --vfs-read-chunk-size 64M \
+  --vfs-read-chunk-size-limit 2G \
+  --transfers 16 \
+  --dir-cache-time 72h \
+  --fast-list \
+  --log-level INFO > /proc/1/fd/1 2>&1
