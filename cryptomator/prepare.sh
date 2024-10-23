df06d84a388297f01ce2b9d0a846cf63482c1bba
yuji01 <59014140+yuji01@users.noreply.github.com>
2024-10-23 19:33:25 +0800

    Create prepare.sh

---
+++ cryptomator/prepare.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# 创建 rclone 配置目录并下载配置文件
+wget --no-check-certificate "$URL" -O /app/rclone.conf &&
+
+# 显示 rclone 配置文件内容到日志
+cat /app/rclone.conf
+
+# 创建 rclone 挂载点
+mkdir -p /rclone-mount
