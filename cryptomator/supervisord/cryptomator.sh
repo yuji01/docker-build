e713a86092cc67718802c04f3519df9a5eb9d132
yuji01 <59014140+yuji01@users.noreply.github.com>
2024-10-24 20:43:23 +0800

    Update cryptomator.sh

--- cryptomator/supervisord/cryptomator.sh
+++ cryptomator/supervisord/cryptomator.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-sleep 5
+# 清理逻辑
+cleanup() {
+    fusermount -u $VAULT_MOUNT
+    exit 0
+}
+
+# 捕获 SIGTERM 信号
+trap cleanup SIGTERM
+
 # 运行解密程序 Cryptomator，将输出也重定向到 stdout
 java -jar /usr/bin/cryptomator.jar \
     --vault $VAULT_NAME=$VAULT_PATH --password $VAULT_NAME=$VAULT_PASS \
