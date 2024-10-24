98681e968e5c70f3c58e8a10b51993393feeaebd
yuji01 <59014140+yuji01@users.noreply.github.com>
2024-10-24 11:00:07 +0800

    Update prepare.sh

--- cryptomator/supervisord/prepare.sh
+++ cryptomator/supervisord/prepare.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-# 创建 rclone 挂载点
-mkdir -p /rclone-mount
+if [ ! -f /app/lock ];then
 
-# 创建 rclone 配置目录并下载配置文件
-wget --no-check-certificate "$URL" -O /app/rclone.conf &&
+  # 创建 rclone 挂载点
+  mkdir -p /rclone-mount
+  
+  # 创建 rclone 配置目录并下载配置文件
+  wget --no-check-certificate "$URL" -O /app/rclone.conf &&
+  
+  # 显示 rclone 配置文件内容到日志
+  cat /app/rclone.conf && touch /app/lock
 
-# 显示 rclone 配置文件内容到日志
-cat /app/rclone.conf
-
+fi
