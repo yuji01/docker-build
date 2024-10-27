79955ae4e52dbb210670b64a490cf7d4a4fc2362
yuji01 <59014140+yuji01@users.noreply.github.com>
2024-10-27 13:17:20 +0800

    Update start.sh

--- ip_derper/start.sh
+++ ip_derper/start.sh
@@ -3,5 +3,7 @@
 # 生成自签10年证书
 openssl req -x509 -newkey rsa:4096 -sha256 -days 36500 -nodes -keyout /app/certs/derp.baidu.com.key -out /app/certs/derp.baidu.com.crt -subj "/CN=derp.baidu.com" -addext "subjectAltName=DNS:derp.baidu.com"
 
+echo "证书已生成"
+
 # 启动
 /app/derper -a :$DERP_PORT -certdir /app/certs -certmode manual -derp -hostname derp.baidu.com -http-port -1 -stun $DERP_STUN -stun-port $DERP_STUN_PORT -verify-clients $DERP_VERIFY_CLIENTS
