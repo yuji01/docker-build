1e4bd156d4c058bda641aa770229ee343463371b
yuji01 <59014140+yuji01@users.noreply.github.com>
2024-11-03 14:37:31 +0800

    Update rclone.sh

--- rclone/rclone.sh
+++ rclone/rclone.sh
@@ -1,7 +1,31 @@
-#! /bin/bash
+#!/bin/sh
+
+if [ ! -f /app/lock ];then
+
+  # 创建 rclone 挂载点
+  mkdir -p /rclone-mount
+  
+  # 创建 rclone 配置目录并下载配置文件
+  wget --no-check-certificate "$URL" -O /app/rclone.conf &&
+  
+  # 显示 rclone 配置文件内容到日志
+  cat /app/rclone.conf && touch /app/lock
+
+fi
+
+# 清理逻辑
+cleanup() {
+    echo "Unmounting /rclone-mount..."
+    fusermount -u /rclone-mount
+    echo "Unmounted /rclone-mount"
+    exit 0
+}
+
+# 捕获 SIGTERM 信号
+trap cleanup SIGTERM
 
 # 启动 rclone，并将输出重定向到 stdout
-exec rclone --config /app/rclone.conf \
+rclone --config /app/rclone.conf \
   --umask=0 mount $REMOTE /mnt \
   --allow-non-empty \
   --allow-other \
