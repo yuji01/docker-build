9e06e78fbf96833732983302ebafbc442976a118
yuji01 <59014140+yuji01@users.noreply.github.com>
2025-12-25 20:46:41 +0800

    Update start.sh

--- frp/frps/start.sh
+++ frp/frps/start.sh
@@ -1,4 +1,5 @@
 #! /bin/sh
 
 # 下载配置文件
-wget -q --no-check-certificate "$URL" -O /app/frps.toml && /app/frps --config /app/frps.toml
+wget -q --no-check-certificate "$URL" -O /app/frps.toml && echo "download complete"
+cat /app/frps.toml && /app/frps --config /app/frps.toml || echo "config error!!"
